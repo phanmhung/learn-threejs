366e09ac1d58b6932c555cf1c025dec1c20982a7
Phan M. Hung <pmhungpro1@gmail.com>
2024-05-07 12:03:05 +0700

    fix: wrong name parameters

--- src/pages/api/profile.tsx
+++ src/pages/api/profile.tsx
@@ -3,9 +3,9 @@ import { supabase } from "@/shared/db";
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const { email, username, image_url } = req.body;
+    const { email, name, image } = req.body;
     const { data, error } = await supabase.from('profiles').upsert({
-      email, username, image_url
+      email, username: name, image_url: image
     }, {
       onConflict: 'email'
     });
